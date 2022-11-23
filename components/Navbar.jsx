99b0a5d85086bfc46e4f67edd991cfede910e90f
Rafiul <ahmadrafiulm@gmail.com>
2022-11-23 15:38:45 +0700

    navbg on scroll

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 function Navbar() {
+  const [navbg, setNavbg] = useState(false);
+
+  const changeNavbg = () => {
+    if (window.scrollY >= 50) {
+      setNavbg(true);
+    } else {
+      setNavbg(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbg);
+
   return (
-    <nav className="fixed w-full z-10">
+    <nav
+      className={
+        navbg
+          ? "fixed w-full z-10 bg-black duration-300 ease-in-out"
+          : "fixed w-full z-10 bg-transparent duration-300 ease-in-out"
+      }
+    >
       <div className="flex w-full justify-center items-center px-[100px]">
         <div className="flex w-full max-w-[1720px] justify-between  py-[35px] ">
           <Image src="/Dips-logo.svg" height={75} width={224} />
